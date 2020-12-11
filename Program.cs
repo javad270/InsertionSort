7df3c4ecc997aeeed27b12edf14643986f74fcc6
javad es <javad.eslami73@gmail.com>
2020-12-11 09:27:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Sorting
+    {
+        public static void InsertionSort(int[] arr)
+        {
+            int j, temp;
+            for (int i = 1; i <= arr.Length - 1; i++)
+            {
+                temp = arr[i];
+                j = i - 1;
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 1, 5, 4, 11, 20, 8, 2, 98, 90, 16 };
+
+            Console.WriteLine("Unsorted array:");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + "  ");
+            
+
+            Sorting.InsertionSort(arr);
+
+            Console.WriteLine("\nSorted array:");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i]+"  ");
+            Console.ReadKey();
+        }
+    }
+}
